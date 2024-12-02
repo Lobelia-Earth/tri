6c8f883499d0dc3a7776fff2950b173eaf32d27b
Vallode <18506096+vallode@users.noreply.github.com>
2024-12-02 18:35:31 +0100

    Fix eslint typescript configuration

--- eslint.config.js
+++ eslint.config.js
@@ -5,21 +5,13 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["dist", "eslint.config.js"] },
+  { files: ["src/**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["lib"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
   {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    files: ["src/**/*.{js,mjs,cjs,ts}"],
     rules: {
       "@typescript-eslint/consistent-type-imports": "error",
     },
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     coverage: {
       exclude: ["lib", ...coverageConfigDefaults.exclude],
+      reporter: ["text"],
     },
   },
 });
